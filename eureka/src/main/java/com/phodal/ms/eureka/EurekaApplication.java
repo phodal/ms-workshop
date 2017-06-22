31a11ac269139c6c88980f50b9b9c44d101a7810
Phodal HUANG <h@phodal.com>
2017-06-22 11:40:54 +0800

    [Eureka] enable serveR

--- eureka/src/main/java/com/phodal/ms/eureka/EurekaApplication.java
+++ eureka/src/main/java/com/phodal/ms/eureka/EurekaApplication.java
@@ -2,10 +2,11 @@ package com.phodal.ms.eureka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class EurekaApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(EurekaApplication.class, args);
 	}
