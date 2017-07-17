2ca39c98e86ee781db9f111b6123b9563597eadc
Phodal HUANG <h@phodal.com>
2017-07-17 17:36:54 +0800

    [GATEWAY] add filter

--- gateway/src/main/java/com/phodal/ms/gateway/GatewayApplication.java
+++ gateway/src/main/java/com/phodal/ms/gateway/GatewayApplication.java
@@ -4,11 +4,18 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableEurekaClient
 @EnableZuulProxy
 public class GatewayApplication {
+
+	@Bean
+	public AuthFilter addAuthFilter() {
+		return new AuthFilter();
+	}
+
 	public static void main(String[] args) {
 		SpringApplication.run(GatewayApplication.class, args);
 	}
