e1f34654c5ff20b4724e6dc921f137c9da99f011
Phodal HUANG <h@phodal.com>
2017-07-17 14:33:36 +0800

    [Services-A] try to get auth info

--- demo-a/src/main/java/com/phodal/ms/demoa/DemoAController.java
+++ demo-a/src/main/java/com/phodal/ms/demoa/DemoAController.java
@@ -1,15 +1,20 @@
 package com.phodal.ms.demoa;
 
+import com.netflix.zuul.context.RequestContext;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletRequest;
+
 @EnableDiscoveryClient
 @SpringBootApplication
+@EnableZuulProxy
 public class DemoAController {
     public static void main(String[] args) {
         SpringApplication.run(DemoAController.class, args);
@@ -20,10 +25,14 @@ public class DemoAController {
 class ServiceInstanceRestController {
 
     @Autowired
+    private HttpServletRequest httpRequest;
+
+    @Autowired
     private DiscoveryClient discoveryClient;
 
     @RequestMapping("/name")
     public String name(){
+        RequestContext ctx = RequestContext.getCurrentContext();
         return "A";
     }
 }
