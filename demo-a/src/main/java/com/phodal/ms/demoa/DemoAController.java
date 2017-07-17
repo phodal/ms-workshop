297acb1e1df3120b9802a66388e01736c2ffc074
Phodal HUANG <h@phodal.com>
2017-07-17 15:58:34 +0800

    [CONFIG] can get value from server

--- demo-a/src/main/java/com/phodal/ms/demoa/DemoAController.java
+++ demo-a/src/main/java/com/phodal/ms/demoa/DemoAController.java
@@ -2,33 +2,36 @@ package com.phodal.ms.demoa;
 
 import com.netflix.zuul.context.RequestContext;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
 
-@EnableDiscoveryClient
-@SpringBootApplication
-@EnableZuulProxy
-public class DemoAController {
-    public static void main(String[] args) {
-        SpringApplication.run(DemoAController.class, args);
-    }
-}
-
 @RestController
-class ServiceInstanceRestController {
+@RefreshScope
+class DemoAController {
 
     @Autowired
     private HttpServletRequest httpRequest;
 
     @Autowired
     private DiscoveryClient discoveryClient;
+
+
+    @Value("${from}")
+    private String from;
+
+    @RequestMapping("/from")
+    public String from() {
+        return this.from;
+    }
 
     @RequestMapping("/name")
     public String name(){
