443f283d6ece4a17a44fd17794be125ba69e3078
Phodal HUANG <h@phodal.com>
2017-07-17 17:56:48 +0800

    [GATEWAY] import redis

--- gateway/src/main/java/com/phodal/ms/gateway/GatewayApplication.java
+++ gateway/src/main/java/com/phodal/ms/gateway/GatewayApplication.java
@@ -5,10 +5,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.context.annotation.Bean;
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
 @SpringBootApplication
 @EnableEurekaClient
 @EnableZuulProxy
+@EnableRedisHttpSession
 public class GatewayApplication {
 
 	@Bean
