deaa01ed4a73412500a7110a98544a98cd6e0b41
Phodal HUANG <h@phodal.com>
2017-07-17 17:11:43 +0800

    [SERVER-A] done for refesh config

--- demo-a/src/main/java/com/phodal/ms/demoa/DemoAController.java
+++ demo-a/src/main/java/com/phodal/ms/demoa/DemoAController.java
@@ -3,12 +3,8 @@ package com.phodal.ms.demoa;
 import com.netflix.zuul.context.RequestContext;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
-import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
