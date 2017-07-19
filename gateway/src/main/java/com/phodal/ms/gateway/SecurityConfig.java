e7f0e43f69f036c499b041dfe0f2f837d863f603
Phodal Huang <h@phodal.com>
2017-07-19 10:13:28 +0800

    [GATEWAY] simplify logic

--- gateway/src/main/java/com/phodal/ms/gateway/SecurityConfig.java
+++ gateway/src/main/java/com/phodal/ms/gateway/SecurityConfig.java
@@ -1,9 +1,6 @@
 package com.phodal.ms.gateway;
 
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.core.annotation.Order;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -11,15 +8,8 @@ import org.springframework.security.web.savedrequest.NullRequestCache;
 import org.springframework.session.web.http.HeaderHttpSessionStrategy;
 import org.springframework.session.web.http.HttpSessionStrategy;
 
-@Configuration
 @EnableWebSecurity
-@Order(1)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
-    @Override
-    protected void configure(AuthenticationManagerBuilder builder) throws Exception {
-        builder.inMemoryAuthentication()
-                .withUser("phodal").password("password").roles("USER");
-    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
