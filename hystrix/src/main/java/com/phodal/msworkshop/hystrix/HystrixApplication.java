2d870a5cf6ca582beb3ea1c2e55becc52d72f35f
Phodal HUANG <h@phodal.com>
2017-07-21 11:09:28 +0800

    [Hystrix] init project

---
+++ hystrix/src/main/java/com/phodal/msworkshop/hystrix/HystrixApplication.java
@@ -0,0 +1,12 @@
+package com.phodal.msworkshop.hystrix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HystrixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HystrixApplication.class, args);
+	}
+}
