473d1d889cf18fd9ecb9df07f082d0849648c6e9
Phodal HUANG <h@phodal.com>
2017-07-21 11:21:49 +0800

    [zipkin] it works

--- zipkin/src/main/java/com/phodal/msworkshop/zipkin/ZipkinApplication.java
+++ zipkin/src/main/java/com/phodal/msworkshop/zipkin/ZipkinApplication.java
@@ -2,7 +2,9 @@ package com.phodal.msworkshop.zipkin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import zipkin.server.EnableZipkinServer;
 
+@EnableZipkinServer
 @SpringBootApplication
 public class ZipkinApplication {
 
